7a15baa611465ddeee45db60d3ceb5268e1f2b2a
dalgyalgim <dalgyalkim@gmail.com>
2023-07-31 18:48:43 -0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+
+const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+    React.useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [value, key]);
+
+    return [value, setValue];
+};
+
+const initialStories = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    }, {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    },
+];
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useStorageState('search', 'React'); 
+    const [stories, setStories] = React.useState(initialStories);
+
+    const handleRemoveStory = (item) => {
+        const newStories = stories.filter((story) => item.objectID !== story.objectID);
+        setStories(newStories);
+    }
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+            <InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch}/>
+            <hr/>
+            <List list={stories} searchTerm={searchTerm} onRemoveItem={handleRemoveStory}/>
+        </div>
+    );
+}
+
+
+const List = ({list, onRemoveItem}) => ( 
+    <ul> {list.map((item) => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>)} </ul>
+);
+
+const Item = ({item, onRemoveItem})  => {
+    const handleRemoveItem = () => {onRemoveItem(item);}
+
+    return (
+    <li>
+        <span>
+            <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+            <button type="button" onClick={handleRemoveItem}>Dismiss</button>
+        </span>
+    </li>
+    );
+}
+
+const InputWithLabel = ({id, label, value, type = "text", onInputChange}) => (
+    <>
+        <label htmlFor={id}>{label}</label>
+        <input id={id} type={type} value={value} onChange={onInputChange}></input>
+        <p>Searching for <strong>{value}.</strong></p>
+    </>
+);
+
+export default App;
